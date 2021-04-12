f37225df4a2cc73719b8d9ff1c8dbbadacf284c8
hostedposted <hostedpostedsite@gmail.com>
2021-04-12 18:47:07 -0400

    Fix bug with findFirstDifferentCharacter function

--- src/index.js
+++ src/index.js
@@ -39,41 +39,42 @@ async function main () {
     while (true) {
         if (pokemon.length === 2) {
             const index = findFirstDifferentCharacter(pokemon[0], pokemon[1]);
-            if (!index) continue;
-            let letter = await LetterInput.fire({
-                html: `What is the ${NumWords[index + 1]} letter in your pokemon's name?`,
-                icon: "question",
-                allowOutsideClick: false
-            });
-            if (!letter.value) {
-                await Swal.fire({
-                    html: "Invalid input.",
-                    icon: "error",
+            if (index) {
+                let letter = await LetterInput.fire({
+                    html: `What is the ${NumWords[index + 1]} letter in your pokemon's name?`,
+                    icon: "question",
                     allowOutsideClick: false
                 });
-                continue;
+                if (!letter.value) {
+                    await Swal.fire({
+                        html: "Invalid input.",
+                        icon: "error",
+                        allowOutsideClick: false
+                    });
+                    continue;
+                }
+                letter = letter.value;
+                if (pokemon[0][index] === letter) {
+                    await Swal.fire({
+                        title: "Pokémon",
+                        html: `The Pokemon Is <b>${pokemon[0]}.</b>`,
+                        allowOutsideClick: false
+                    });
+                } else if (pokemon[1][index] === letter) {
+                    await Swal.fire({
+                        title: "Pokémon",
+                        html: `The Pokemon Is <b>${pokemon[1]}.</b>`,
+                        allowOutsideClick: false
+                    });
+                } else {
+                    await Swal.fire({
+                        title: "Pokémon",
+                        html: "Pokemon could not be found.",
+                        allowOutsideClick: false
+                    });
+                }
+                break;
             }
-            letter = letter.value;
-            if (pokemon[0][index] === letter) {
-                await Swal.fire({
-                    title: "Pokémon",
-                    html: `The Pokemon Is <b>${pokemon[0]}.</b>`,
-                    allowOutsideClick: false
-                });
-            } else if (pokemon[1][index] === letter) {
-                await Swal.fire({
-                    title: "Pokémon",
-                    html: `The Pokemon Is <b>${pokemon[1]}.</b>`,
-                    allowOutsideClick: false
-                });
-            } else {
-                await Swal.fire({
-                    title: "Pokémon",
-                    html: "Pokemon could not be found.",
-                    allowOutsideClick: false
-                });
-            }
-            break;
         }
         if (pokemon.length === 1) {
             await Swal.fire({
